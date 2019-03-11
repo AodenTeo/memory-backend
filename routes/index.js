d5f2dc9772e73d349459ca281c1795630854bd50
Aoden Teo <aodenteo@gmail.com>
2019-03-11 15:46:07 +0800

    Created email send function

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,14 @@ let score = 0;
 let numberOfRuns = 0;
 let timeBetween;
 let randomness;
-
-
-
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+        user: 'aodenteo@gmail.com',
+        pass: 'i_am_a_memory_bot'
+    }
+});
 /* GET home page. */
 router.all('/*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -28,13 +33,13 @@ router.post('/settings', (req, res, next) => {
   console.log(numberOfDigits);
   console.log(setFlashes);
   console.log(setNum);
-  res.send('This is the obligatory response');  
+  res.send('This is the obligatory response');
 })
 router.post('/answers/:number', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   answer = req.params.number
-  res.send(answer);  
+  res.send(answer);
 })
 router.post('/result/:number', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -60,7 +65,7 @@ router.get('/information', async (req, res, next) => {
   await res.send({
     numberOfDigits: numberOfDigits,
     flashTime: flashTime,
-    setNum: setNum, 
+    setNum: setNum,
     setFlashes: setFlashes,
     timeBetween: timeBetween,
     randomness: randomness
@@ -86,6 +91,21 @@ router.get('/final', (req, res, next) => {
     numberOfSets: setNum,
     score: score
   });
+  const mailOptions = {
+  from: 'aodenmemorybot@gmail.com', // sender address
+  to: 'joannestaunton2@gmail.com', // list of receivers
+  subject: 'Someone just used your memory app!', // Subject line
+  html: '<p>Hooray! Someone just used your memory training app! That\'s amazing. Yours Truly, Computer Bot. </p>'// plain text body
+};
+
+ transporter.sendMail(mailOptions, function (err, info) {
+  if(err) {
+    console.log(err)
+  }
+  else {
+    console.log(info);
+  }
+});
   score = 0;
   numberOfRuns = 0;
 })
